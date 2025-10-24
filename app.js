b5ced707ad2e2d4049f0bd7a0212f64115d3f76b
Faseeh Hussain <syedfaseehnaqvi2@gmail.com>
2025-10-25 03:19:49 +0500

    js add

---
+++ app.js
@@ -0,0 +1,11 @@
+// alert("helo! i am a alert box");
+// confirm("this procces!")
+
+// let person=prompt("plz enter your name?", "ali rahi tharparkar" )
+let age = 20;
+
+if (age >= 18) {
+  console.log("Adult");  // true hone par chalega
+} else {
+  console.log("Minor");
+}
